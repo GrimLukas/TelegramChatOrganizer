417c668edf54e066b0ab61e492b84b7c57957706
Hanzomain69 <hanzomain69xxx69@gmail.com>
2019-06-06 09:56:22 +0200

    test succesfull

--- test_db.js
+++ test_db.js
@@ -1,3 +1,4 @@
 const db_test = require('./DbUtils.js');
 
-db_test.insertOne({name:'test', value:'this is a test'});
+//db_test.insertOne({name:'test', value:'this is a test'});
+//test was succesfull, deleted the insertion afterwards
