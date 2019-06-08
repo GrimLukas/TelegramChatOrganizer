a52e6fe1461692e2066b8e6a2e2c60865de600ee
Hanzomain69 <hanzomain69xxx69@gmail.com>
2019-06-08 11:33:16 +0200

    added intervall to save messages

--- telegram_api.js
+++ telegram_api.js
@@ -1,46 +1,50 @@
 const request = require('https');
 
 const MongoClient = require('mongodb').MongoClient;
-    const assert = require('assert');
+const assert = require('assert');
+const express = require('express');
 
-    // Connection URL
-    const url = 'mongodb://localhost:27017';
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-    // Database Name
-    const dbName = 'Archive';
+// Database Name
+var db;
+const dbName = 'Archive';
 
-    // Create a new MongoClient
-    const client = new MongoClient(url);
+// Create a new MongoClient
+const client = new MongoClient(url);
 
-    //Use connect method to connect to the Server
-    client.connect(function(err) {
+//Use connect method to connect to the Server
+client.connect(function (err) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
-    const db = client.db(dbName);
-    request.get('https://api.telegram.org/bot861420868:AAF8exAJl1IZCGDxNGJd9w_EqQ0fEOH0sWA/getUpdates', (response) => {
-    let data = '';
-
-    response.on('data', (chunk) => {    //empfängt die einzelnen Packages und fasst sie in einer Variable zusammen
-            data+=chunk;
-        })
-
-    response.on('end', () => {
-        let update = JSON.parse(data); //parse into JSON to access specific fields
-        var nachricht;
-        update.result.forEach(message => {      //takes the chat name, message-id,-user,-date and -text out of the JSON file and puts it into 'nachricht'
-            nachricht={
-            chat_name: message.message.chat.title,
-            msg_id: message.message.message_id,
-            msg_user: message.message.from.username,
-            msg_date: new Date(1000*message.message.date),  
-            msg_text: message.message.text
-            };
-            db.collection('Messages').insertOne(JSON.parse(JSON.stringify(nachricht)));
-        })
-    client.close();
-    })
-})
-});
+    db = client.db(dbName);
+    setInterval(getUpdates, 5000);  //alle 5 Sekunden in Datenbank speichern
     
+})
+            
+function getUpdates() {
+    request.get('https://api.telegram.org/bot861420868:AAF8exAJl1IZCGDxNGJd9w_EqQ0fEOH0sWA/getUpdates', (response) => {
+        let data = '';
 
+        response.on('data', (chunk) => {    //empfängt die einzelnen Packages und fasst sie in einer Variable zusammen
+            data += chunk;
+        })
+
+        response.on('end', () => {
+            let update = JSON.parse(data); //parse into JSON to access specific fields
+            var nachricht;
+            update.result.forEach(message => {      //takes the chat name, message-id,-user,-date and -text out of the JSON file and puts it into 'nachricht'
+                nachricht = {
+                    chat_name: message.message.chat.title,
+                    msg_id: message.message.message_id,
+                    msg_user: message.message.from.username,
+                    msg_date: new Date(1000 * message.message.date),
+                    msg_text: message.message.text
+                };
+                db.collection('Messages').insertOne(JSON.parse(JSON.stringify(nachricht))); //nachricht gets saved into Database
+            })
+        })
+    })
+}
